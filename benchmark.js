35483a6d769719e5d0a9b9f48557290b6be71592
Vincent LE GOFF <vince.legoff@gmail.com>
2021-10-21 10:47:03 +0200

    feat: add benchmarks

--- benchmark.js
+++ benchmark.js
@@ -1,16 +1,30 @@
 const benchmark = require('benchmark')
 const suite = new benchmark.Suite()
-const URI = require('./')
-URI.parse('https://example.com')
+const fastifyURI = require('./')
+const urijs = require('uri-js')
 
-suite.add('uri', function () {
-  URI.parse('https://example.com')
+// Initialization as there is a lot to parse at first
+// eg: regexes
+fastifyURI.parse('https://example.com')
+urijs.parse('https://example.com')
+
+suite.add('fastifyURI: parse', function () {
+  fastifyURI.parse('https://example.com')
 })
-suite.add('IPv4', function () {
-  URI.parse('//10.10.10.10')
+suite.add('urijs: parse', function () {
+  urijs.parse('https://example.com')
 })
-suite.add('IPv6', function () {
-  URI.parse('//[2001:db8::7]')
+suite.add('fastifyURI: IPv4', function () {
+  fastifyURI.parse('//10.10.10.10')
+})
+suite.add('urijs: IPv4', function () {
+  urijs.parse('//10.10.10.10')
+})
+suite.add('fastifyURI: IPv6', function () {
+  fastifyURI.parse('//[2001:db8::7]')
+})
+suite.add('urijs: IPv6', function () {
+  urijs.parse('//[2001:db8::7]')
 })
 
 suite.on('cycle', cycle)
