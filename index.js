6c97e357f7e0bc15a3dbbcf1c54c3b21c995d47a
Frazer Smith <frazer.dev@outlook.com>
2023-09-10 10:54:27 +0100

    perf: use `node:` prefix to bypass require.cache call for builtins (#69)
    See https://github.com/fastify/fastify-static/pull/407

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const URL = require('url')
+const URL = require('node:url')
 const { normalizeIPv6, normalizeIPv4, removeDotSegments, recomposeAuthority, normalizeComponentEncoding } = require('./lib/utils')
 const SCHEMES = require('./lib/schemes')
 
