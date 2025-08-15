033500efe82168032deac6f677a749910940ae4c
Burkov Egor <eburkov@rvision.ru>
2025-08-15 09:56:53 +0300

    Add test for issue

--- test/util.test.js
+++ test/util.test.js
@@ -2,7 +2,8 @@
 
 const test = require('tape')
 const {
-  stringArrayToHexStripped
+  stringArrayToHexStripped,
+  removeDotSegments
 } = require('../lib/utils')
 
 test('stringArrayToHexStripped', (t) => {
@@ -19,5 +20,19 @@ test('stringArrayToHexStripped', (t) => {
 
   testCases.forEach(([input, expected]) => {
     t.same(stringArrayToHexStripped(input[0], input[1]), expected)
+  })
+})
+
+// Just fixtures, because this function already tested by resolve
+test('removeDotSegments', (t) => {
+  const testCases = []
+  // https://github.com/fastify/fast-uri/issues/139
+  testCases.push(['WS:/WS://1305G130505:1&%0D:1&C(XXXXX*)))))))XXX130505:UUVUaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa$aaaaaaaaaaaa13a',
+    'WS:/WS://1305G130505:1&%0D:1&C(XXXXX*)))))))XXX130505:UUVUaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa$aaaaaaaaaaaa13a'])
+
+  t.plan(testCases.length)
+
+  testCases.forEach(([input, expected]) => {
+    t.same(removeDotSegments(input), expected)
   })
 })
