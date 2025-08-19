8351063e056e01d42a535fcc055ff4e240deb82d
Aras Abbasi <aras.abbasi@googlemail.com>
2025-08-19 13:10:27 +0200

    improve stringArrayToHexStripped

--- test/util.test.js
+++ test/util.test.js
@@ -8,18 +8,18 @@ const {
 
 test('stringArrayToHexStripped', (t) => {
   const testCases = [
-    [[['0', '0', '0', '0']], ''],
-    [[['0', '0', '0', '0'], false], ''],
-    [[['0', '0', '0', '0'], true], '0'],
-    [[['0', '1', '0', '0'], false], '100'],
-    [[['1', '0', '0', '0'], false], '1000'],
-    [[['1', '0', '0', '0'], true], '1000']
+    [['0', '0', '0', '0'], ''],
+    [['0', '0', '0', '1'], '1'],
+    [['0', '0', '1', '0'], '10'],
+    [['0', '1', '0', '0'], '100'],
+    [['1', '0', '0', '0'], '1000'],
+    [['1', '0', '0', '1'], '1001'],
   ]
 
   t.plan(testCases.length)
 
   testCases.forEach(([input, expected]) => {
-    t.same(stringArrayToHexStripped(input[0], input[1]), expected)
+    t.same(stringArrayToHexStripped(input), expected)
   })
 })
 
